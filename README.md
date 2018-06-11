ad089bee7d86778536eb6ef96c4c227f205afe16
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-06-11 10:53:13 -0300

    Creando readme e instalando psycopg2-binary

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Django api helper
+[![CircleCI](https://circleci.com/gh/pilas-engine/pilas-engine-backend.svg?style=svg)](https://circleci.com/gh/pilas-engine/pilas-engine-backend)
--- pilas/tests/tests_api_proyecto.py
+++ pilas/tests/tests_api_proyecto.py
@@ -4,4 +4,6 @@ class APIProyectoTests(APITestCase):
 
     def test_puede_listar_los_proyectos(self):
         response = self.client.get('/api/proyectos')
+        print(response)
+        print(response.data)
         self.assertEqual(len(response.data['results']), 0)
