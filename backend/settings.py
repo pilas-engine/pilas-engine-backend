d059c416392af48fd77413fca1524c76277c1235
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-07-06 00:01:04 -0300

    Bump version: 0.0.7 → 0.0.8

--- backend/settings.py
+++ backend/settings.py
@@ -2,7 +2,7 @@ import os
 import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-VERSION = "0.0.7"
+VERSION = "0.0.8"
 
 SECRET_KEY = '+^&h3f(g%*@j!b@i2dc9yd&jzwg9cw(xnw$j&@n+hay(fh)eoc'
 DEBUG = True
