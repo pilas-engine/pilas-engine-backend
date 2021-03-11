d4458c57ffebc7a1066ab34c82ea66e8a223de27
Hugo Ruscitti <hugoruscitti@gmail.com>
2021-03-10 21:32:26 -0300

    Quitando el campo serializado de la vista edición de proyectos.

--- pilas/admin_classes/proyecto.py
+++ pilas/admin_classes/proyecto.py
@@ -8,6 +8,10 @@ class ProyectoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'hash', 'url', 'ver_codigo')
     search_fields = ('nombre', )
 
+    def change_view(self, request, object_id, extra_context=None):       
+        self.exclude = ('codigo_serializado', )
+        return super(ProyectoAdmin, self).change_view(request, object_id, extra_context)
+
     def url(self, obj):
         baseurl = os.environ.get('BACKEND_URL')
         url = os.path.join(baseurl, "proyecto", str(obj.hash))
