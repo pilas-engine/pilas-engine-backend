83249ae0b937d945e221035882042676b05a5d47
Hugo Ruscitti <hugoruscitti@gmail.com>
2021-12-24 17:54:55 -0300

    Elimina api-helper.

--- django-api-helper/templates/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from aplicacion.models.modelo import Modelo
-
-class ModeloAdmin(admin.ModelAdmin):
-    model = Modelo
-    list_display = ('id', 'nombre')
-    search_fields = ('nombre', )
--- django-api-helper/templates/tests.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.test import APITestCase
-
-class APIModeloTests(APITestCase):
-
-    def test_puede_listar_los_modelo_plural(self):
-        response = self.client.get('/api/modelo_plural')
-        self.assertEqual(len(response.data['results']), 0)
